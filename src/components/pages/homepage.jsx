a802aa843f7c96cf1b640aa55f3c6fa424f73569
HoangMinh23062002 <minh.hoang23@student.passerellesnumeriques.org>
2023-02-23 15:52:14 +0700

    deployment

--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Slider from "../containers/homepage/Slider";
-import Plans from "../containers/homepage/Plans";
-import Ownblog from "../containers/homepage/Ownblog";
-import Intro from "../containers/homepage/Intro";
-import Appdl from "../containers/homepage/Appdl";
+import Header from "../..//components/pages/Header";
+import Footer from "../..//components/pages/Footer";
+import Slider from "../..//components/containers/homepage/slider";
+import Intro from "../..//components/containers/homepage/intro";
+import Ownblog from "../..//components/containers/homepage/ownblog";
+import Plans from "../..//components/containers/homepage/plans";
+import Appdl from "../..//components/containers/homepage/appdl";
+
 // import Footer from "../containers/";
-import Footer from "./Footer";
-import Header from "./Header";
+//    
 
 export default function Homepage() {
   return (
